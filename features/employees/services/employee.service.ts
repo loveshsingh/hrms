8dd691efa68131b3c7841f50848e5fe8c72c6478
loveshsingh <loveshsinghsuperfine@gmail.com>
2026-07-15 23:42:46 +0530

    feat: add employee service layer

---
+++ features/employees/services/employee.service.ts
@@ -0,0 +1,59 @@
+import { ApiError } from "@/lib/api-error";
+import { employeeRepository } from "../repositories/employee.repository";
+
+import type {
+  CreateEmployeeInput,
+  UpdateEmployeeInput,
+} from "../validators/employee.validator";
+
+export class EmployeeService {
+  async create(data: CreateEmployeeInput) {
+    const existingEmployeeCode = await employeeRepository.findByEmployeeCode(
+      data.employeeCode,
+    );
+
+    if (existingEmployeeCode) {
+      throw new ApiError(409, "Employee code already exists.");
+    }
+
+    const existingEmail = await employeeRepository.findByEmail(data.email);
+
+    if (existingEmail) {
+      throw new ApiError(409, "Email already exists.");
+    }
+
+    return employeeRepository.create(data);
+  }
+
+  async findById(id: string) {
+    const employee = await employeeRepository.findById(id);
+
+    if (!employee) {
+      throw new ApiError(404, "Employee not found.");
+    }
+
+    return employee;
+  }
+
+  async findMany() {
+    return employeeRepository.findMany();
+  }
+
+  async update(id: string, data: UpdateEmployeeInput) {
+    await this.findById(id);
+
+    return employeeRepository.update(id, data);
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    return employeeRepository.softDelete(id);
+  }
+
+  async count() {
+    return employeeRepository.count();
+  }
+}
+
+export const employeeService = new EmployeeService();
